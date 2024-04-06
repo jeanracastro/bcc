e1d67186f0d99431a4f8815746a0c68165e46253
Jen <114189141+jeanracastro@users.noreply.github.com>
2024-04-05 21:30:19 -0700

    Create e.js
    javascript1 hw

---
+++ e.js
@@ -0,0 +1,14 @@
+var questionOne = prompt("What is your name?");
+document.getElementById("blankOne").innerHTML = questionOne;
+
+var questionOne = prompt("What would you name a pink cat?");
+document.getElementById("blankTwo").innerHTML = questionOne;
+
+var questionOne = prompt("Now, make up a sound that a pink cat would make...");
+document.getElementById("blankThree").innerHTML = questionOne + '!!' ;
+
+
+var questionOne = prompt("Text. ");
+document.getElementById("blankFour").innerHTML = questionOne ;
+
+
