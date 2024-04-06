e0d28d2df75cfe3f9bd308426076d7beef33bf2e
Jen <114189141+jeanracastro@users.noreply.github.com>
2024-04-05 21:39:27 -0700

    Update e.js

--- e.js
+++ e.js
@@ -7,4 +7,5 @@ document.getElementById("blankTwo").innerHTML = questionOne;
 var questionOne = prompt("Now, make up a sound that a pink cat would make...");
 document.getElementById("blankThree").innerHTML = questionOne + '!!' ;
 
-
+var questionOne = prompt("You have colonized this space. Name your new planet.");
+document.getElementById("blankFour").innerHTML = questionOne + '!!' ;
