6dc439a8be8bced92abef864ff40e13528272d9c
Jen <114189141+jeanracastro@users.noreply.github.com>
2024-04-05 21:31:31 -0700

    Update e.js

--- e.js
+++ e.js
@@ -8,7 +8,3 @@ var questionOne = prompt("Now, make up a sound that a pink cat would make...");
 document.getElementById("blankThree").innerHTML = questionOne + '!!' ;
 
 
-var questionOne = prompt("Text. ");
-document.getElementById("blankFour").innerHTML = questionOne ;
-
-
